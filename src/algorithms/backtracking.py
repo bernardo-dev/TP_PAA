3fad98607b5fc23fae0b65667f2158cc9316a8a7
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2026-01-31 16:24:12 -0300

    updates Bernardo

--- src/algorithms/backtracking.py
+++ src/algorithms/backtracking.py
@@ -1,6 +1,40 @@
+import sys
 import time
 import os
 import glob
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils import read_instance, salvar_resultado
+
+def execute (instance_path):
+    W_max, V_max, itens = read_instance(instance_path)
+    
+    if W_max is None:
+        print(f"Erro ao processar {instance_path}, pulando...\n")
+        return
+
+    print(f"\tCapacidade W: {W_max}, Capacidade V: {V_max}, Número de Itens: {len(itens)}")
+
+    inicio = time.time()
+    
+    lucro_max, itens_escolhidos = backtracking_solver(itens, W_max, V_max, len(itens))
+    
+    fim = time.time()
+    tempo_execucao = fim - inicio
+
+    print(f"\t\tLucro Máximo: {lucro_max}")
+    print(f"\t\tItens escolhidos (IDs): {sorted(itens_escolhidos)}")
+    print(f"\t\tTempo de execução: {tempo_execucao:.6f} segundos")
+    
+    salvar_resultado(
+        'solutions/backtracking/' + os.path.basename(instance_path).replace('.txt', '_results.csv'),
+        len(itens),
+        W_max,
+        V_max,
+        lucro_max,
+        tempo_execucao,
+        sorted(itens_escolhidos)
+    )
+
 
 class Item:
     def __init__(self, id, peso, volume, valor):
@@ -33,6 +67,9 @@ def backtracking_solver(itens, capacidade_w, capacidade_v, n):
     """
     Função recursiva de Backtracking
     Retorna: (melhor_valor, lista_de_itens_escolhidos)
+    0 = peso
+    1 = volume
+    2 = valor
     """
     # Caso base: sem itens ou capacidades esgotadas
     if n == 0 or (capacidade_w == 0 and capacidade_v == 0):
@@ -41,25 +78,25 @@ def backtracking_solver(itens, capacidade_w, capacidade_v, n):
     item_atual = itens[n-1]
 
     # restrição: item não cabe (por peso ou volume)
-    if item_atual.peso > capacidade_w or item_atual.volume > capacidade_v:
+    if item_atual[0] > capacidade_w or item_atual[1] > capacidade_v:
         return backtracking_solver(itens, capacidade_w, capacidade_v, n-1)
 
     else:
         # Incluir o item (Soma valor, subtrai capacidades)
         val_com, itens_com = backtracking_solver(
             itens, 
-            capacidade_w - item_atual.peso, 
-            capacidade_v - item_atual.volume, 
+            capacidade_w - item_atual[0], 
+            capacidade_v - item_atual[1], 
             n-1
         )
-        val_com += item_atual.valor
+        val_com += item_atual[2]
 
         # Não incluir o item
         val_sem, itens_sem = backtracking_solver(itens, capacidade_w, capacidade_v, n-1)
 
         # Decisão: Retorna o que der maior valor
         if val_com > val_sem:
-            return val_com, itens_com + [item_atual.id]
+            return val_com, itens_com + [n-1]
         else:
             return val_sem, itens_sem
 
--- src/main.py
+++ src/main.py
@@ -1,44 +1,43 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(file))))
 from gerador_instancias.gerar import get_experiment_data
-from .algorithms import dynamic_programming as dp
-from .algorithms import branch_and_bound as bb
+from src.algorithms import dynamic_programming as dp
 from consts import INSTANCE_BASE_PATH
 from algorithms import backtracking as bt
 
 def dynamic_programming_experiment(experiment_number, combination, instance):
     print("\tExecutando experimento de programação dinâmica...")
 
-    instance_path = f'{INSTANCE_BASE_PATH}/experimento_{experiment_number}/combinacao{combination}_{instance}.txt'
-    dp.execute(instance_path, experiment_number)
+    # Construct the absolute path to the project root
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(file)))
+    instance_path = f'{INSTANCE_BASEPATH}/experimento{experimentnumber}/combinacao{combination}{instance}.txt'
+    dp.execute(instance_path)
 
-def branch_and_bound_experiment(experiment_number, combination, instance):
-    print("\tExecutando experimento de branch and bound...")
-
-    instance_path = f'{INSTANCE_BASE_PATH}/experimento_{experiment_number}/combinacao{combination}_{instance}.txt'
-    bb.execute(instance_path, experiment_number)
-
-def backtracking_experiment():
+def backtracking_experiment(experiment_number, combination, instance):
     print("\tExecutando experimento de backtracking...")
 
+    # Construct the absolute path to the project root
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(file)))
+    instance_path = f'{INSTANCE_BASEPATH}/experimento{experimentnumber}/combinacao{combination}{instance}.txt'
+    bt.execute(instance_path)
+
+
+
 def run_experiment(experiment_number):
-    print("-----------------------------------")
     print(f"Iniciando Experimento {experiment_number}")
     print("-----------------------------------")
-
 
     for combination in range(1, 4):
         for instance in range(1, 11):
             W, V, n = get_experiment_data(experiment_number, combination)
             print(f"Processando instância {instance} - Combinação {combination}: W={W}, V={V}, n={n}")
 
-            dynamic_programming_experiment(experiment_number, combination, instance)
-            branch_and_bound_experiment(experiment_number, combination, instance)
-            backtracking_experiment()
-   
+            # dynamic_programming_experiment(experiment_number, combination, instance)
+            backtracking_experiment(experiment_number, combination, instance)
 
-if __name__ == "__main__":
+
+if name == "main":
     run_experiment(1)
-    #run_experiment(2)
-    #run_experiment(3) 
+    run_experiment(2)
+    run_experiment(3)  # Altere o número para 2 ou 3 para outros experimentos
